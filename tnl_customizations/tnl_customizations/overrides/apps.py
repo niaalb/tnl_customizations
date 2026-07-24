b75ed39230c38fe3ffa928d404a9316edf569cbe
Nihal <nihal@tnl.sa>
2026-07-25 00:12:22 +0530

    fix: added some unadded changes

--- tnl_customizations/tnl_customizations/overrides/apps.py
+++ tnl_customizations/tnl_customizations/overrides/apps.py
@@ -1,7 +1,25 @@
 import frappe
 from frappe.apps import get_apps as original_get_apps
 
+APP_REBRANDING = {
+    "raven": {
+        "title": "Discussion",
+        "logo": "/assets/tnl_customizations/images/discussion-logo.png",
+    },
+    "hrms": {
+        "title": "People",
+        "logo": "/assets/tnl_customizations/images/people-logo.svg",
+    },
+}
+
 
 @frappe.whitelist()
 def get_apps():
-    return original_get_apps()
+    apps = original_get_apps()
+
+    for app in apps:
+        branding = APP_REBRANDING.get(app["name"])
+        if branding:
+            app.update(branding)
+
+    return apps
